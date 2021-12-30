444c8db17ab17d851e0b267778f13a8c5466ef46
Morozov <MorozovPrav@gmail.com>
2021-12-30 17:17:17 +0200

    update search response. add category data

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -27,7 +27,7 @@ class ArticleController extends Controller
         return response()->json([
             'status' => 'success',
             'data' => $content
-        ]);
+        ])->setEncodingOptions('ass');
     }
 
     /**
@@ -57,8 +57,15 @@ class ArticleController extends Controller
      */
     public function getArticleListSearchResponse(Request $request)
     {
-        $data = OneArticleModel::query()->select('id', 'article_title', 'main_image', 'create_date', 'author_id')
-            ->where('article_title', 'LIKE',  "%$request->search%")->get();
+        $data = OneArticleModel::query()->select('id', 'article_title', 'main_image', 'create_date', 'author_id', 'article_preview_description')
+            ->where('article_title', 'LIKE',  "%$request->search%")
+            ->with(['oneCategory' => function($q){
+                $q->select(
+                    'one_category_models.id',
+                    'category_title',
+                );
+            }])
+            ->get();
 
         $content = [];
         foreach ($data as $article){
